b34b0dcf1add290ba2f351b080ab269c036ff219
martinhc82 <martinhc82@hotmail.com>
2019-09-25 09:46:36 -0300

    2

---
+++ imprimir.php
@@ -0,0 +1,22 @@
+<?php
+/*echo($_POST["nombre"]);
+
+foreach ($_POST as $key => $value) {
+  echo"<br> $value";
+}
+var_dump(getallheaders());
+*/
+
+var_dump($_SERVER);
+echo "<br>";
+var_dump($_FILES);
+echo "<br>";
+var_dump($_REQUEST);
+echo "<br>";
+var_dump($_SESSION);
+echo "<br>";
+var_dump($_COOKIE);
+echo "<br>";
+var_dump($_GLOBALS);
+
+ ?>
